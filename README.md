d1f0dc2f22c43e952f8086e1e47518a7d497a4c2
Christian Mendes Pereira <christianp3m@gmail.com>
2022-05-31 21:44:32 -0300

    Redux installed and tested in Login page

--- README.md
+++ README.md
@@ -20,3 +20,11 @@ Launches the test runner in the interactive watch mode.\
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
 ### `npm install react-router-dom@v5`
+
+
+### `npm install @reduxjs/toolkit react-redux`
+
+
+### `npm install redux`
+
+### `npm install react-redux`
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { saveName } from '../store/user';
 
 function Login() {
+
   const [name, setName] = useState('');
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <form>
@@ -11,13 +16,16 @@ function Login() {
             data-testid="input-player-name"
             placeholder="Nome"
             value={name}
-            onChange={(e) => setName(e.target.value)}
+            onChange={(e) => {setName(e.target.value)}}
           />
 
           <button
             type="submit"
             data-testid="btn-play"
-            onClick={ (e) => e.preventDefault() }
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch(saveName(name));
+            } }
           >
             Jogar
           </button>
